340ed905f2eb97ef125e8c3ca7b90b8d04ba8e36
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-20 16:33:28 +0530

    Create result detail component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState("");
 	const [searchApi, results, errorMessage] = useResults();
+
+	const filterResultsByPrice = (price) => {
+		return results.filter((result) => result.price === price);
+	};
 
 	return (
 		<View>
@@ -16,6 +21,9 @@ const SearchScreen = () => {
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>We have found {results.length} results</Text>
+			<ResultsList title='Cost Effective' results={filterResultsByPrice("$")} />
+			<ResultsList title='Bit Pricier' results={filterResultsByPrice("$$")} />
+			<ResultsList title='Big Spender' results={filterResultsByPrice("$$$")} />
 		</View>
 	);
 };
